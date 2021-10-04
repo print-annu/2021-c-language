ae76a281b21d4c1a28b6e480b27c0b8f419bdc4f
print-annu <72179753+print-annu@users.noreply.github.com>
2021-10-04 21:08:58 +0530

    Add files via upload

---
+++ binartodec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int n = 1010;
+    int result = 0;
+    int i = 0;
+    while (n>0)
+    {
+        int last = n %10;
+        result += last * (pow(2,i));
+        n = n/10;
+        i++;
+    }
+    printf("%d ",result);
+          
+}
